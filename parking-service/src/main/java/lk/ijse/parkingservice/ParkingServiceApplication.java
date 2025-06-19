2008417ad575ed47020d5d050c228b1d9288b289
sherulfernando2000 <sherul.dhanushka@gmail.com>
2025-06-19 16:44:52 +0530

    payment-service 1.1 done

---
+++ parking-service/src/main/java/lk/ijse/parkingservice/ParkingServiceApplication.java
@@ -0,0 +1,15 @@
+package lk.ijse.parkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceApplication.class, args);
+    }
+
+}
